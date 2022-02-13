da40916bc33db9c167877c7d1f259b7cb788c019
workbear <zou2895083202@gmail.com>
2022-02-13 18:46:13 +0800

    Initial commit

---
+++ mss-auth/src/main/java/top/workbear/mssauth/MssAuthApplication.java
@@ -0,0 +1,13 @@
+package top.workbear.mssauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssAuthApplication.class, args);
+    }
+
+}
---
+++ mss-auth/src/test/java/top/workbear/mssauth/MssAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package top.workbear.mssauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
